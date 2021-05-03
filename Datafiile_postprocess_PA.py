22a74fc14923205109a3ca3cef989ae0a10f2834
sungmoonkimeu <sungmoon.kim@umons.ac.be>
2021-05-03 17:58:20 +0200

    UPdated with new data~

---
+++ Datafiile_postprocess_PA.py
@@ -0,0 +1,29 @@
+"""Analysing datafiles from polarimeter POD-101
+
+"""
+
+import string
+import os
+path_dir = 'Data_pol'
+file_list = os.listdir(path_dir)
+
+for nn in range(len(file_list)):
+    fn = path_dir + "//" + file_list[nn]
+
+    # test
+    # fn = 'Data2//9_30deg_hibi_Upper.txt'
+    # fn2 = 'Data2_edited//9_30deg_hibi_Upper2.txt'
+    tmp = file_list[nn].split('.')
+    # Removing file header
+    # Data starting from n th line
+
+    databeginning = 4
+    fn2 = 'Data_pol_edited//' + tmp[0] + '_edited.txt'
+    with open(fn) as fp:
+        for i, line in enumerate(fp):
+            if i > databeginning:
+                print(line)  # 26th line
+                with open(fn2, 'a') as fp2:
+                    fp2.writelines(fp)
+
+#mydatax = pd.read_table('Data2//9_30deg_hibi_Upper.txt')
---
+++ main_polarimeter.py
@@ -0,0 +1,101 @@
+"""Analysing datafiles from Polarimeter device
+"""
+
+import time
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from numpy import cos, pi, mat, ones, zeros, sin, einsum, append, arange, array, cumsum, argmin, sqrt, arcsin, arctan, \
+    tan, random, column_stack, savetxt, loadtxt
+from numpy.linalg import norm, eig, matrix_power
+import matplotlib.ticker
+from matplotlib.ticker import (MaxNLocator,
+                               FormatStrFormatter, ScalarFormatter)
+from scipy.interpolate import interp1d
+import matplotlib.transforms
+import pandas as pd
+# matplotlib.rcParams['mathtext.fontset'] = 'custom'
+# matplotlib.rcParams['font.family'] = 'serif'
+# matplotlib.rcParams['font.serif'] = 'Computer Modern'
+# matplotlib.pyplot.title(r'ABC123 vs $\mathrm{ABC123}^{123}$')
+# matplotlib.pyplot.title(r'ABC123 vs $\mathrm{ABC123}^{123}$')
+# plt.rcParams['font.family']='sans-serif'
+# plt.rcParams['font.sans-serif']='Comic Sans MS'
+
+
+#from py_pol.jones_vector import Jones_vector, degrees
+#from py_pol.stokes import Stokes, create_Stokes
+#from py_pol.drawings import draw_stokes_points, draw_poincare, draw_ellipse
+
+a = cos(135)
+
+# noinspection PyPep8Naming
+class OOMFormatter(matplotlib.ticker.ScalarFormatter):
+    def __init__(self, order=0, fformat="%1.1f", offset=True, mathText=True):
+        self.oom = order
+        self.fformat = fformat
+        matplotlib.ticker.ScalarFormatter.__init__(self, useOffset=offset, useMathText=mathText)
+
+    def _set_order_of_magnitude(self):
+        self.orderOfMagnitude = self.oom
+
+    def _set_format(self, vmin=None, vmax=None):
+        self.format = self.fformat
+        if self._useMathText:
+            self.format = r'$\mathdefault{%s}$' % self.format
+
+
+import os
+
+# os.chdir('C:/Users/Iter/PycharmProjects/loaddata')
+# os.chdir('C:/Users/SMK/PycharmProjects/loaddata/venv/')
+# os.chdir('C:/Users/SMK/PycharmProjects/loaddata/venv/')
+
+path_dir = 'Data_pol_edited'
+file_list = os.listdir(path_dir)
+
+'''
+for nn in range(len(file_list)):
+    fn = path_dir + "//" + file_list[nn]
+    data = pd.read_table(fn)
+    time = data['Time (ns)']
+    signal = data['Amplitude (dB)']
+    fig, ax = plt.subplots(figsize=(6, 5))
+    ax.plot(time, signal, lw='1', label=file_list[nn])
+    ax.legend(loc="upper right")
+    ax.set(xlim=(124, 134), ylim=(-135, -120))
+plt.show()
+'''
+
+a = arange(30, 360, 30)
+count = 0
+fig, ax = plt.subplots(4, figsize=(6, 5))
+
+fn2 = path_dir + "//10Hz_edited.txt"
+data = pd.read_table(fn2, delimiter=r"\s+")
+time = pd.to_numeric(data['Index'])
+S0 = pd.to_numeric(data['S0(mW)'])
+S1 = pd.to_numeric(data['S1'])
+S2 = pd.to_numeric(data['S2'])
+S3 = pd.to_numeric(data['S3'])
+
+ax[0].plot(time, S0)
+ax[1].plot(time, S1)
+ax[2].plot(time, S2)
+ax[3].plot(time, S3)
+
+
+'''
+for nn in a:
+
+    fn2 = path_dir + "//" + "9turns_" + str(nn) + "deg_Upper_edited.txt"
+    data = pd.read_table(fn2)
+    time = data['Time (ns)']
+    signal = data['Amplitude (dB)']
+    ax[count].plot(time, signal, lw='1', label="9turns_"+str(nn)+"deg")
+    ax[count].legend(loc="upper right")
+    ax[count].set(xlim=(124, 134), ylim=(-135, -120))
+    count = count+1
+'''
+plt.show()
