a81a41c71ac74d8e60b3afd2bd1d16dc558e4ca5
sungmoonkimeu <sungmoon.kim@umons.ac.be>
2022-01-26 00:04:57 +0100

    Fig#4 update!

--- drawpoincare_ver_AGV.py
+++ drawpoincare_ver_AGV.py
@@ -45,6 +45,7 @@ def PS3(shot):
     ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
     ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
     ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+
     # no ticks
     ax.set_xticklabels([])
     ax.set_yticklabels([])
@@ -60,28 +61,32 @@ def PS3(shot):
     y = sprad * np.outer(np.sin(u), np.sin(v))
     z = sprad * np.outer(np.ones(np.size(u)), np.cos(v))
 
+
     ax.plot_surface(x, y, z,
                     color='w',  # (0.5, 0.5, 0.5, 0.0),
-                    edgecolor='k',
+                    #edgecolor='k',
+                    edgecolor=(3/256, 3/256, 3/256),
                     linestyle=(0, (5, 5)),
                     rstride=3, cstride=3,
-                    linewidth=.5, alpha=0.8, shade=0)
+                    linewidth=.5, alpha=0.8, shade=
+                    0)
+
 
     # main circles
-    ax.plot(np.sin(u), np.cos(u), np.zeros_like(u), 'g-.', linewidth=0.75)  # equator
+    #ax.plot(np.sin(u), np.cos(u), np.zeros_like(u), 'g-.', linewidth=0.75)  # equator
     #    ax.plot(np.sin(u), np.zeros_like(u), np.cos(u), 'b-', linewidth=0.5)
     #    ax.plot(np.zeros_like(u), np.sin(u), np.cos(u), 'b-', linewidth=0.5)
 
     # axes and captions
     amp = 1.3 * sprad
-    ax.plot([-amp, amp], [0, 0], [0, 0], 'k-.', lw=1, alpha=0.5)
-    ax.plot([0, 0], [-amp, amp], [0, 0], 'k-.', lw=1, alpha=0.5)
-    ax.plot([0, 0], [0, 0], [-amp, amp], 'k-.', lw=1, alpha=0.5)
+    ax.plot([-amp, amp], [0, 0], [0, 0], 'k-.', lw=1, alpha=0.5, zorder=1)
+    ax.plot([0, 0], [-amp, amp], [0, 0], 'k-.', lw=1, alpha=0.5, zorder=1)
+    ax.plot([0, 0], [0, 0], [-amp, amp], 'k-.', lw=1, alpha=0.5, zorder=1)
 
     distance = 1.3 * sprad
     ax.text(distance, 0, 0, '$S_1$', fontsize=18)
-    ax.text(0, distance, 0, '$S_2$', fontsize=18)
-    ax.text(0, 0, distance, '$S_3$', fontsize=18)
+    ax.text(0, -distance, 0, '$S_2$', fontsize=18)
+    ax.text(0, 0, -distance, '$S_3$', fontsize=18)
 
     # points
     px = [1, -1, 0, 0, 0, 0]
@@ -89,7 +94,7 @@ def PS3(shot):
     pz = [0, 0, 0, 0, 1, -1]
 
     ax.plot(px, py, pz,
-            color='black', marker='o', markersize=4, alpha=1.0, linewidth=0)
+            color='black', marker='o', markersize=4, alpha=1.0, linewidth=0, zorder=3)
     #
 
     max_size = 1.05 * sprad
@@ -98,13 +103,16 @@ def PS3(shot):
     ax.set_zlim(-max_size, max_size)
 
     #    plt.tight_layout()            #not compatible
-    ax.view_init(elev=90 / np.pi, azim=90 / np.pi)
+    #ax.view_init(elev=-21, azim=-54)
+    ax.view_init(elev=-158, azim=126)
     #    ax.view_init(elev=0/np.pi, azim=0/np.pi)
 
     #    ax.set_title(label = shot, loc='left', pad=10)
     ax.set_title(label="  " + shot, loc='left', pad=-10, fontsize=8)
 
     #    ax.legend()
+
+    ax.set_box_aspect([1, 1, 1])
 
     return ax, fig
 
@@ -116,7 +124,6 @@ def main():
     plt.close("all")
     plt_fmt, plt_res = '.png', 330  # 330 is max in Word'16
     plt.rcParams["axes.titlepad"] = 20  # offset for the fig title
-    plt.rcParams["figure.autolayout"] = True  # tight_layout
     #  plt.rcParams['figure.constrained_layout.use'] = True  # fit legends in fig window
     fsize = 18
     plt.rc('font', size=fsize)  # controls default text sizes
@@ -140,7 +147,9 @@ def main():
     Sv = create_Stokes('Output_S')
     Out = create_Stokes('Output_S2')
 
-    for nn in range(len(file_list)):
+    #for nn in range(len(file_list)):
+    for nn in range(1):
+
 
         fn2 = path_dir + "//" + file_list[nn]
         count = 0
@@ -160,12 +169,15 @@ def main():
         cm = np.linspace(0, 1, len(S1))  # color map
         cm[-1] = 1.3
 
-        ax.scatter3D(S1, S2, S3, zdir='z', marker='o', s=4, c=cm,
-                     alpha=1.0, label='F1', cmap="brg")
+        #ax.scatter3D(S1, S2, S3, zdir='z', marker='o', s=4, c=cm, zorder=555,
+        #             alpha=1, label='F1', cmap="cool")
+
+        ax.plot(S1, S2, S3,
+                marker='o', markersize=4, alpha=1.0, linewidth=0, zorder=3)
 
         #ax.scatter3D(D1, D2, D3, zdir='z', marker='+', s=6, c=cm,
         #             alpha=0.6, label='F2', cmap="cool")
-        ax.legend()
+        #ax.legend()
 
 if (__name__ == "__main__"):
     main()
