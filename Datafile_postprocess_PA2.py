bce59b7b9248b137b9f68eb10948396b1242e140
sungmoonkimeu <sungmoon.kim@umons.ac.be>
2022-01-26 11:56:34 +0100

    Laser stability & Polarization controller stability test code

---
+++ Datafile_postprocess_PA2.py
@@ -0,0 +1,47 @@
+"""Analysing datafiles from polarimeter POD-101
+
+"""
+
+import string
+import os
+
+#foldername = 'Const_Freq_Polarimeter'
+#foldername = '/010921_HIBI_Const_Disp_Polarimeter/RHC'
+#foldername = '/Data_Vib_2_(Hibi_losen_fasten)/1_RHC_Fasten'
+foldername = '/Laser stability test'
+
+
+#path_dir = os.getcwd() + '/Data_Vib_1_(Oscillo_Polarimeter)/' + foldername
+path_dir = os.getcwd() + foldername
+file_list = os.listdir(path_dir)
+
+def createfolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory')
+
+
+target_path = path_dir + "_edited"
+createfolder(target_path)
+
+for nn in range(len(file_list)):
+    fn = path_dir + "//" + file_list[nn]
+    tmp = file_list[nn].split('.')
+    # Removing file header
+    # Data starting from n th line
+
+    databeginning = 11   # 4 is normal, sometimes it should be changed to 5
+    fn2 = path_dir + '_edited//' + tmp[0] + '_edited.txt'
+    eol = 3600              # end of line
+    with open(fn) as fp:
+        for i, line in enumerate(fp):
+            if databeginning < i < eol+databeginning+2:
+                line2 = line.replace("--", "\t")
+                with open(fn2, 'a') as fp2:
+                    fp2.write(line2)
+
+
+
+
